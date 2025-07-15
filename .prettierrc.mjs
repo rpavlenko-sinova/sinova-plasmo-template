11a4a0312c10bba176af31ac655a6969c1fb3165
rpavlenko-sinova <rpavlenko.sinova@gmail.com>
2025-07-15 15:36:29 +0300

    add: npm prettier config

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,8 +1,19 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+import config from '@rpavlenko.sinova/prettier-config';
+
 export default {
-  printWidth: 120,
-  trailingComma: 'all',
-  singleQuote: true,
-  singleAttributePerLine: true,
-  plugins: ['prettier-plugin-tailwindcss'],
+  ...config,
+  plugins: ['@ianvs/prettier-plugin-sort-imports', 'prettier-plugin-tailwindcss'],
+  importOrder: [
+    '<BUILTIN_MODULES>', // Node.js built-in modules
+    '<THIRD_PARTY_MODULES>', // Imports not matched by other special words or groups.
+    '', // Empty line
+    '^@plasmo/(.*)$',
+    '',
+    '^@plasmohq/(.*)$',
+    '',
+    '^~(.*)$',
+    '',
+    '^[./]',
+  ],
 };
