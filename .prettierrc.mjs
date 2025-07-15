6fda9026f31bf584d45d8f3dd197df980e5fcc37
rpavlenko-sinova <rpavlenko.sinova@gmail.com>
2025-07-15 16:40:09 +0300

    add: changed plugin import

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -3,7 +3,7 @@ import config from '@rpavlenko.sinova/prettier-config';
 
 export default {
   ...config,
-  plugins: ['@ianvs/prettier-plugin-sort-imports', 'prettier-plugin-tailwindcss'],
+  plugins: [...(config.plugins || []), '@ianvs/prettier-plugin-sort-imports'],
   importOrder: [
     '<BUILTIN_MODULES>', // Node.js built-in modules
     '<THIRD_PARTY_MODULES>', // Imports not matched by other special words or groups.
